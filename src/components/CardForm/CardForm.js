a6cabd4aaab70539dc599770df640c85cffbf587
Szymon Kluka <szymonkluka@onet.pl>
2022-12-16 17:16:18 +0100

    new commit2

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,13 +2,18 @@ import styles from './CardForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
+import shortid from 'shortid';
 
-const CardForm = props => {
+const CardForm = (props) => {
+    const dispatch = useDispatch();
     const [title, setTitle] = useState('');
+
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId);
+        dispatch({ type: 'ADD_CARD', payload: { id: shortid(), columnId: props.columnId, title: title } });
         setTitle('');
+
     };
 
     return (
